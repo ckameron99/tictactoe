cc7c267b173d5935948be8fc52a3c4d0ca807c84
Cameron Mackenzie <ckameron99@gmail.com>
2022-02-05 14:23:18 +0000

    remove backend option of network opponent

--- multiplayer.py
+++ multiplayer.py
@@ -3,7 +3,10 @@ from minimax import MiniMax
 
 
 class Multiplayer:
+    """Class to store functions and data relating to players"""
     def __init__(self, board):
+        """Initializes the multiplayer class"""
+        # define possible opponents
         self.opponents = {
             1: PlayLocal(board),
             2: MiniMax(board),
@@ -11,25 +14,31 @@ class Multiplayer:
         self.board = board
 
     def getOpponent(self):
+        """Get's the user's choice of opponent"""
         print(
             "There are several opponents you can play against:\n",
             "1. Local multiplayer\n",
             "2. Against the computer\n",
             # "3. Against another player on the local network",
         )
+
+        # get user selection
         selection = ""
-        while not re.match("^[1-3]$", selection):
+        while not re.match("^[1-2]$", selection):  # TODO network option
             selection = input("Please enter the number of your selection:")
         return int(selection)
 
     def setOpponent(self, selection):
+        """Set the choice of opponent"""
         self.opponent = self.opponents[selection]
 
     def getMove(self, currentMove, nextMove):
+        """Returns the player's desired move"""
         return self.opponent.getMove(currentMove, nextMove)
 
 
 class PlayLocal:
+    """Class to store functions and data relating to a local player"""
     def __init__(self, board):
         self.board = board
 
